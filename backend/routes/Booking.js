63a6dbb446f6e2d07e681c97b1fade524536192b
KUMAIL261103 <kumailghadi2611@gmail.com>
2024-06-29 15:36:00 +0530

    added router code

---
+++ backend/routes/Booking.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const {auth, isAdmin,isManager,isUser} = require("../middlewares/Auth");
+const {
+    getAllBookings,
+    getBookingByUserId,
+    getBookingByFacilityId,
+    createBooking,
+    approveBooking
+}= require("../controllers/Booking");
+
+
+
+router.get("/getAllBookings",auth,isAdmin, getAllBookings);
+
+router.get("/getBookingByUserId",auth,isUser, getBookingByUserId)
+
+router.get("/getBookingByFacilityId",auth,isAdmin,getBookingByFacilityId);
+
+router.post("/createBooking",auth,isUser, createBooking);
+
+router.post("/approveBooking",auth,isAdmin,approveBooking);
+
+module.exports = router;

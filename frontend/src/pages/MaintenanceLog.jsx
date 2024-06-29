298badf7771c4b6a2d4bba69cf418bf2970bdb23
khushibansal13 <khushibansal1302@gmail.com>
2024-06-29 16:45:42 +0530

    add navbar in maintenance log

--- frontend/src/pages/MaintenanceLog.jsx
+++ frontend/src/pages/MaintenanceLog.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import UserNavbar from '../components/UserNavbar';
 
 const MaintenanceLog = () => {
   const maintenanceLogs = [
     { id: 1, date: '2023-01-01', description: 'Replaced air filters' },
     { id: 2, date: '2023-02-01', description: 'Checked HVAC system' },
-    // Add more maintenance log entries here
   ];
 
   return (
+    <>
+    <UserNavbar
+     label1="Home"
+     label2="Booking"
+     label3="Schedule"
+     label4="Contact"/>
     <div className="flex flex-col items-center min-h-screen bg-white p-4">
       <div className="w-3/4 max-w-md bg-black text-white p-4 rounded-lg">
         <h2 className="text-xl mb-4">Maintenance Log</h2>
@@ -21,6 +27,8 @@ const MaintenanceLog = () => {
         </ul>
       </div>
     </div>
+    
+    </>
   );
 };
 

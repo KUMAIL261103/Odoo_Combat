ef92a28e3a2caccb38dee642e1c1bc1839369827
Prem2310 <premrpatel.cse21@aii.ac.in>
2024-06-29 16:32:25 +0530

    usenavbar signup redirection

--- frontend/src/components/UserNavbar.jsx
+++ frontend/src/components/UserNavbar.jsx
@@ -1,14 +1,13 @@
 import PropTypes from "prop-types";
-
+import { useNavigate } from "react-router-dom";
 export default function UserNavbar({ label1, label2, label3, label4 }) {
+  const navigate = useNavigate();
   return (
     <div>
       <nav className="bg-slate-950 shadow-md py-[2vh]">
         <div className="w-[90vw] mx-auto flex justify-between items-center">
           {/* Logo */}
-          <div className="text-[2.8vw] font-bold text-white">
-            Logoo
-          </div>
+          <div className="text-[2.8vw] font-bold text-white">Logoo</div>
           {/* Navigation Links */}
           <div className="flex space-x-[1.6vw]">
             <a href="#" className="text-[1.6vw] text-white hover:text-gray-400">
@@ -26,8 +25,11 @@ export default function UserNavbar({ label1, label2, label3, label4 }) {
           </div>
           {/* Login/Signin */}
           <div className="text-[1.5vw] flex items-center text-white">
-            <button className="border border-light-green px-[2vw] py-[1vh] rounded-xl hover:bg-light-green hover:text-black">
-              Sign In
+            <button
+              onClick={() => navigate("/signup")}
+              className="border border-light-green px-[2vw] py-[1vh] rounded-xl hover:bg-light-green hover:text-black"
+            >
+              Sign Up
             </button>
           </div>
         </div>

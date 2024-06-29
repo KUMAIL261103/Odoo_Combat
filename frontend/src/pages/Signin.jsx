04d4bbaf9a7646082d0480bfdbcd638421d2af94
Prem2310 <premrpatel.cse21@aii.ac.in>
2024-06-29 16:16:57 +0530

    login signup redirection with input field

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -5,10 +5,34 @@ import { LoginInputBox } from "../components/LoginInputBox";
 import { LoginSubHeading } from "../components/LoginSubHeading";
 import { MdOutlineMail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
-
+import { useState } from "react";
 import signinPageImg from "../assets/signinPage.avif";
 
 export const Signin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = () => {
+    const requestBody = {
+      email,
+      password,
+    };
+    console.log("Request Body:", requestBody); // Add this line to debug
+    async function logindata() {
+      const response = await fetch("http://localhost:3000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
+      const data = await response.json();
+      if (data.success) {
+        window.location.href = "/home";
+      }
+      console.log(data);
+    }
+    logindata();
+  };
   return (
     <>
       <div className="bg-slate-950 h-screen pt-20 pl-20 pr-20 flex">
@@ -20,14 +44,18 @@ export const Signin = () => {
               <LoginInputBox
                 placeholder="Email"
                 icon={<MdOutlineMail color="white" />}
+                value={email}
+                onChange={setEmail}
               />
               <LoginInputBox
                 placeholder="Password"
                 icon={<RiLockPasswordLine color="white" />}
+                value={password}
+                onChange={setPassword}
               />
             </div>
             <div className="mt-14">
-              <LoginButton label={"Login"} />
+              <LoginButton label={"Login"} onClick={handleLogin} />
             </div>
             <LoginBottomWarning
               label={"Dont have an account?"}

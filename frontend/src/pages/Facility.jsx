91e83799ef6a1760430592c0e06f0e7cb6e2a76d
KUMAIL261103 <kumailghadi2611@gmail.com>
2024-07-09 10:05:48 +0530

    fixed and optimised facilities available on curr Date logic

--- frontend/src/pages/Facility.jsx
+++ frontend/src/pages/Facility.jsx
@@ -6,16 +6,50 @@ import LandingNavbar from "../components/LandingNavbar";
 import ManagerNavbar from "../components/ManagerNavbar";
 import AdminNavbar from "../components/AdminNavbar";
 
+const isUsedDateCheck = (isUsedDate) => {
+  if(isUsedDate.length === 0)return false;
+  let start = 0;
+  let end = isUsedDate.length - 1;
+  const year = new Date.getFullYear();
+  const month = String(new Date.getMonth() + 1).padStart(2, '0');
+  const day = String(new Date.getDate()).padStart(2, '0');
+  const currentDate = `${year}-${month}-${day}`;
+  while(start <= end){
+    let mid = Math.floor((start + end) / 2);
+    let isUsedDate = isUsedDate[mid];
+    if(currentDate === isUsedDate){
+      return true;
+    }
+    if(currentDate < isUsedDate){
+      start = mid + 1;
+    }else{
+      end = mid - 1;
+    }
+  }
+  return false;
+
+}
+const isAvailable = (facilities) => {
+  const availableFacilities = [];
+  for(let i=0;i<facilities.length;i++){  
+    if(isUsedDateCheck(facilities[i].isUsedDate)==false){
+      availableFacilities.push(facilities[i]);
+    }  
+  }
+  return availableFacilities;
+}
 export const Facility = () => {
   const [facilities, setFacilities] = useState([]);
-
   useEffect(() => {
     const fetchFacilities = async () => {
       try {
+        const backendUrl = import.meta.url.VITE_API_URL || "http://localhost:3000";
         const response = await axios.get(
-          "https://odoo-combat-cgs8.onrender.com/api/facilities/getAllFacilities"
-        );
-        setFacilities(response.data.facilities);
+          `${backendUrl}/api/facilities/getAllFacilities`
+         );
+        const availableFacilities = isAvailable(response.data.facilities);
+        console.log(response.data.facilities);
+        setFacilities(availableFacilities);
       } catch (error) {
         console.error("Error fetching facilities:", error);
       }
@@ -60,6 +94,7 @@ export const Facility = () => {
       <div className="flex flex-wrap gap-4">
         {facilities.length > 0 ? (
           facilities.map((facility, index) => (
+            
             <div className="flex justify-between " key={index}>
               <FacilityCard
                 img={facility.image}

26e7b4429e5e8e2f17455f052bceb890593dc439
khushibansal13 <khushibansal1302@gmail.com>
2024-07-09 17:11:30 +0530

    added welcome message to the navbar

--- frontend/src/components/AdminNavbar.jsx
+++ frontend/src/components/AdminNavbar.jsx
@@ -1,6 +1,8 @@
 import logo from "../assets/logo.png";
 
 export default function AdminNavbar() {
+  const user = JSON.parse(sessionStorage.getItem("user")) || undefined;
+
   // let token = localStorage.getItem("token");
   // const navigate = useNavigate();
   return (
@@ -19,6 +21,18 @@ export default function AdminNavbar() {
             <a href="/maintenance" className="text-[1.5vw] text-white hover:text-gray-400">
               Maintenance
             </a>
+          </div>
+          <div className="text-[1.3vw] flex items-center text-white">
+          {user ? (
+              <span className="flex items-center justify-center text-white font-light opacity-90 capitalize">welcome, {user.firstName}</span>
+            ) : (
+            <button
+              onClick={() => navigate("/signup")}
+
+              className={`border border-light-green px-[2vw] py-[1vh] rounded-xl hover:bg-light-green hover:text-black ${token!=undefined?"hidden":"visible"}`} >
+              Sign Up
+            </button>
+             )}
           </div>
           
           {/* Login/Signin */}

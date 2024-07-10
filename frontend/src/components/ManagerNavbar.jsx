ec700ac86b978752af189c9b11f10d169bfd141b
KUMAIL261103 <kumailghadi2611@gmail.com>
2024-07-11 00:27:15 +0530

    added logout btn in admin and manager navbars

--- frontend/src/components/ManagerNavbar.jsx
+++ frontend/src/components/ManagerNavbar.jsx
@@ -1,9 +1,14 @@
 import logo from "../assets/logo.png";
-
+import { useNavigate } from "react-router-dom";
 export default function MangerNavbar() {
   const user = JSON.parse(sessionStorage.getItem("user")) || undefined;
-  // let token = localStorage.getItem("token");
-  // const navigate = useNavigate();
+  const logout = () => {
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("user");
+    window.location.reload();
+  };
+   let token = sessionStorage.getItem("token");
+   const navigate = useNavigate();
   return (
     <div>
       <nav className="bg-slate-950 shadow-md -h-[1vh]">
@@ -20,8 +25,12 @@ export default function MangerNavbar() {
            
           </div>
         <div className="text-[1.3vw] flex items-center text-white">
-          {user ? (
-              <span className="flex items-center justify-center text-white font-light opacity-90 capitalize">welcome, {user.firstName}</span>
+          {user ?  (
+              <div className="flex flex-col place-items-center  gap-2 mt-4">
+
+                <span className="flex items-center  justify-center  text-white font-light opacity-90 capitalize">welcome, {user.firstName}</span>
+                <button className=" px-3  rounded-md bg-slate-600 text-gray-white text-lg" onClick={()=>logout()}>Logout</button>
+              </div>
             ) : (
             <button
               onClick={() => navigate("/signup")}
